c41b8544ed67e5542c9cccd5a3a2cf3e7d699ea6
tolatolatop <tolatolatop@gmail.com>
2022-08-31 05:07:03 +0800

    适配git信息多种编码 重构git log cmd

--- awake_sheep/git.py
+++ awake_sheep/git.py
@@ -18,15 +18,21 @@ async def shell(cmd, cwd):
 
     stdout, stderr = await proc.communicate()
 
-    return proc.returncode, stdout.decode(), stderr.decode()
+    return proc.returncode, stdout, stderr
 
 
 async def git_log_repo(repo_path: pathlib.Path, since=None):
-    pretty = "###%H:%ae:%ct%n%B"
-    if since:
-        cmd = f'git log --pretty={pretty} --since="{since}" '
+    cmd = get_git_log_cmd(None, since)
+
+    ret, stdout, stderr = await shell(cmd, repo_path)
+    if ret == 0:
+        return get_commit_info(stdout)
     else:
-        cmd = f'git log --pretty={pretty}'
+        raise RuntimeError(cmd, stderr)
+
+
+async def git_log_file(file_path: pathlib.Path, repo_path: pathlib.Path, since=None):
+    cmd = get_git_log_cmd(file_path, since)
 
     ret, stdout, stderr = await shell(cmd, repo_path)
     if ret == 0:
@@ -36,9 +42,30 @@ async def git_log_repo(repo_path: pathlib.Path, since=None):
 
 
 def get_commit_info(stdout_string):
-    regex = r'###([a-z0-9]+):([^:]+):(\d+)\n(.*?)(?=###)'
+    regex = rb'###([a-z0-9]+):([^:]+):(\d+)\n(.*?)(?=###)?'
     result = re.findall(regex, stdout_string, re.DOTALL)
     return result
 
 
+def get_git_log_cmd(file_path: pathlib.Path, since=None):
+    pretty = "###%H:%ae:%ct%n%B"
+    if file_path is None:
+        file_path_args = ''
+    else:
+        file_path_args = file_path
 
+    if since:
+        if since.isdigit():
+            since_args = f'-{since}'
+        else:
+            since_args = f"--since={since}"
+    else:
+        since_args = ""
+    cmd = f'git log {file_path_args} --pretty={pretty} {since_args}'
+    return cmd
+
+
+if __name__ == "__main__":
+    f = git_log_repo(pathlib.Path('.'), '1')
+    res = asyncio.run(f)
+    print(res)
